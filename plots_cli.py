380d7326ffc9357467fe2b7c2c89866b9e32f13e
Zeyad <zwa2861@rit.edu>
2021-10-29 20:25:09 +0400

    6th commit

--- plots_cli.py
+++ plots_cli.py
@@ -1,17 +1,18 @@
 
 def main():
 # this funtion will ask the user to quit or not
-    quit_x = input("Enter a command or 'quit' to quit. ")
+    quit_x = input("Enter a command or 'quit' to quit: ")
     z = quit_x.split(" ")
 
-    if quit_x[0] == str("quit"):
+    if z[0] == str("quit"):
          quit()
          print("Goodbye!")
 
     else:
         try:
             if quit_x != str("quit"):
-                print("Enter a command or enter 'quit' to quit")
+                print("   Failed input")
+                print("   Enter a command or enter 'quit' to quit please: ")
                 main()
 
         except:
@@ -21,7 +22,7 @@ def main():
 def quit():
     # asks the user if sure or not
     print("Are you sure?")
-    yes = input("If yes enter Y ")
+    yes = input("If yes enter Y:- ")
     if yes == str("Y"):
         return True
     else:
@@ -35,4 +36,5 @@ def quit():
 
 
 if __name__ == '__main__':
-    quit()
+    main()
+
