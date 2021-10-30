99a7e286e3f43b813611c22e5777a698381f61ba
Zeyad <zwa2861@rit.edu>
2021-10-30 19:40:13 +0400

    8th commit

--- plots_cli.py
+++ plots_cli.py
@@ -39,52 +39,52 @@ def quit():
         return False
 
 
-def student_average(a_string):
+def student_average(stu_string):
 
 # a_string = "stu, filename, firstname, lastname"
-   a_string = ["stu", "filename", "firstname", "lastname", plotter.plot(trace_plot=True)]
+   stu_string = ["stu", "filename", "firstname", "lastname", plotter.plot(trace_plot=True)]
    try:
-     if (a_string[4] == True):
+     if (stu_string[4] == True):
       print("“Plot finished (window may be hidden).")
 
-     if (a_string[4] == False):
+     if (stu_string[4] == False):
       print("Plot failed (no such student)")
 
 
-     if (a_string[0] == None):
+     if (stu_string[0] == None):
          print("Usage: stu <filename> <first name> <last name>")
-     if (a_string[1] == None):
+     if (stu_string[1] == None):
          print("No such file: foo.csv")
-     if (a_string[2] == None):
+     if (stu_string[2] == None):
          print("Usage: stu <filename> <first name> <last name>")
-     if (a_string[3] == None):
+     if (stu_string[3] == None):
          print("Usage: stu <filename> <first name> <last name>")
 
    except:
        return
 
 
-def print_average(b_string):
-    b_string = ["avg","filename", "gradeitem"]
+def print_average(avg_string):
+    avg_string = ["avg","filename", "gradeitem"]
     try:
-        if (b_string[0] == None):
+        if (avg_string[0] == None):
             print("Usage: avg <filename> <grade item>")
-        if (b_string[1] == None):
+        if (avg_string[1] == None):
             print("Usage: avg <filename> <grade item>")
-        if (b_string[2] == None):
+        if (avg_string[2] == None):
             print("Usage: avg <filename> <grade item>")
 
-        if (b_string[1] == None):
+        if (avg_string[1] == None):
             print("No such file: foo.csv")
 
-        if (b_string[2] != int()):
+        if (avg_string[2] != int()):
             print("Grade item must be a number")
 
     except:
         return
 
 
-
+def class_average(class_string):
 
 
 
