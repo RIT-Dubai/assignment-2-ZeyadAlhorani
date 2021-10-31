4f6de25539ef00f3688d5fbef3f2fac4b894f27f
Zeyad <zwa2861@rit.edu>
2021-10-31 13:11:59 +0400

    8th commit

--- plots_cli.py
+++ plots_cli.py
@@ -1,16 +1,23 @@
-
+import csv
 import plotter
 
 def main():
 # this funtion will ask the user to quit or not
-    quit_x = input("Enter a command: ")
-    z = quit_x.split(" ")
+    command = input(">> ")
+    z = command.split()
 
-    if (quit_x == "stu"):
-       student_average()
 
-    if (quit_x == "avg"):
+    if (z[0] == "stu"):
+       student_average(["stu",r'C:\Users\Zeyad\Desktop\GCIS.123.600-assignment2-sample.csv','zeyad','bonita'])
+
+    if (command == "avg"):
         print_average()
+
+    if (command == "cavg"):
+        class_average()
+
+    if (command == "help"):
+        help()
 
     if z[0] == str("quit"):
          quit()
@@ -18,14 +25,17 @@ def main():
 
     else:
         try:
-            if quit_x != str("quit"):
-                print("   Failed input")
-                print("   Enter a command or enter 'quit' to quit please: ")
-                main()
+            if z[0] == str("quit"):
+                quit()
+                print("Goodbye!")
+
+            #elif command != str("quit"):
+                #print("   Failed input")
+                #print("   Enter a command or enter 'quit' to quit please: ")
+                #main()
 
         except:
            return
-
 
 
 
@@ -40,33 +50,48 @@ def quit():
 
 
 def student_average(stu_string):
-
 # a_string = "stu, filename, firstname, lastname"
-   stu_string = ["stu", "filename", "firstname", "lastname", plotter.plot(trace_plot=True)]
-   try:
-     if (stu_string[4] == True):
-      print("“Plot finished (window may be hidden).")
+   stu_string = ["stu",r"C:\Users\Zeyad\Desktop\GCIS.123.600-assignment2-sample.csv", "Zeyad", "Bonita", plotter.plot(trace_plot=True)]
+   namefile = [1]
+   if len(stu_string) != 4:
+       print("Usage: stu <filename> <first name> <last name>")
 
-     if (stu_string[4] == False):
-      print("Plot failed (no such student)")
-
-
-     if (stu_string[0] == None):
-         print("Usage: stu <filename> <first name> <last name>")
-     if (stu_string[1] == None):
+   if (stu_string[1] == None):
          print("No such file: foo.csv")
-     if (stu_string[2] == None):
-         print("Usage: stu <filename> <first name> <last name>")
-     if (stu_string[3] == None):
-         print("Usage: stu <filename> <first name> <last name>")
 
-   except:
-       return
+   else:
+    try:
+       with open(r"C:\Users\Zeyad\GCIS.123.600-assignment2-sample.csv") as namefile:
+        csv_reader = csv.reader(namefile)
+       fisrtname = stu_string[2]
+       lastname = stu_string[3]
+       row = []
+       for row in csv_reader:
+           while fisrtname in row:
+               if lastname in row:
+                   print(row)
+                   plotter.init("my graph", "X-axis", "Y-axis")
+    except:
+
+     if (stu_string[4] == True):
+       print("“Plot finished (window may be hidden).")
+     #else:
+       #stu_string[4] == False
+       #print("Plot failed (no such student)")
+
+
+
 
 
 def print_average(avg_string):
-    avg_string = ["avg","filename", "gradeitem"]
+    avg_string = ["avg","filename", "gradeitem", plotter.plot_point("70", "98", "red")]
+
+    if len(avg_string) != 4:
+       print("Usage: stu <filename> <first name> <last name>")
+
     try:
+        if (avg_string == True):
+            print("Average:")
         if (avg_string[0] == None):
             print("Usage: avg <filename> <grade item>")
         if (avg_string[1] == None):
@@ -85,6 +110,30 @@ def print_average(avg_string):
 
 
 def class_average(class_string):
+    class_string = ["cavg", "filename", plotter.plot_data_points(data_points, dot_color, trace_plot=True)]
+
+    try:
+        if (class_string == True):
+            print("Plot is finished (window may be hidden).")
+
+        if (class_string[0] == False):
+            print("Usage: cavg <filename>")
+        if (class_string[1] == False):
+            print("No such file: foo.csv")
+
+    except:
+        return
+
+
+def help():
+    print("stu <filename> <first name> <last name> - plot student grades")
+    print("cavg <filename> - plot class average")
+    print("avg <filename> <number> - prints the average for the grade item")
+    print("quit - quits")
+    print("help - displays this message")
+
+
+
 
 
 
