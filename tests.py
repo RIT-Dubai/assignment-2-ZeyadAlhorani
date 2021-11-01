6bdb581569c45b0d04d09682b2e0f94bf6385eec
Zeyad <zwa2861@rit.edu>
2021-11-01 19:51:58 +0400

    37th commit

--- tests.py
+++ tests.py
@@ -4,6 +4,10 @@ import plotter
 
 def test_student_average():
 # test to make sure the input is correct
+    stu_correct = ["stu",r'C:\Users\Zeyad\Desktop\GCIS.123.600-assignment2-sample.csv','zeyad','bonita']
+    stu_wrong = ["stu",r'C:\Users\Zeyad\Desktop\GCIS.123.600-assignment2-sample.csv','zey','bonita']
+    assert plots_cli.student_average(stu_correct) == True
+    assert plots_cli.student_average(stu_wrong) == False
 
 
 def test_print_average():
