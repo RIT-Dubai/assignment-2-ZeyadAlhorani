fabad989755c8d8e7283b6ba4d8ef7fb7a4a9720
Zeyad <zwa2861@rit.edu>
2021-11-01 19:38:19 +0400

    34th commit

--- tests.py
+++ tests.py
@@ -3,11 +3,12 @@ import plotter
 
 
 def test_student_average():
+# test to make sure the input is correct
 
 
 def test_print_average():
 
 
 def test_class_average():
-
+# test to make sure output are sorted and correct
 
